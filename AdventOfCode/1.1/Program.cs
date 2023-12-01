6dd0c9f090bea6b455579face6feb8a1fe15daf0
Patryk Konieczny <pat366@gmail.com>
2023-12-01 19:15:31 +0100

    Day 1 challenge 1

---
+++ AdventOfCode/1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+string[] inputLines = File.ReadAllLines(inputFilePath);
+int sum = 0;
+foreach (string line in inputLines)
+{
+    char firstDigit = line.First(c => char.IsDigit(c));
+    char lastDigit = line.Last(c => char.IsDigit(c));
+    sum += int.Parse($"{firstDigit}{lastDigit}");
+}
+Console.WriteLine(sum);
+Console.ReadLine();
