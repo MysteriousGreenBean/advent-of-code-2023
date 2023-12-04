5c82f38d7c5803912c28a8df1d6060aa77e20097
Patryk Konieczny <pat366@gmail.com>
2023-12-04 19:46:11 +0100

    Day 4 Challenge 1

---
+++ AdventOfCode/4.1/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1
+{
+    internal class Card
+    {
+        public int[] WinningNumbers { get; }
+        public int[] MarkedNumbers { get; }
+
+        public Card(string cardLine)
+        {
+            string[] cardParts = cardLine.Split(':', '|');
+            WinningNumbers = cardParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            MarkedNumbers = cardParts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+
+        public int GetScore()
+        {
+            int winningAmount = WinningNumbers.Count(MarkedNumbers.Contains);
+            if (winningAmount == 0)
+                return 0;
+            return (int)Math.Pow(2, winningAmount - 1);
+        }
+    }
+}
---
+++ AdventOfCode/4.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using _4._1;
+
+Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+string[] inputLines = File.ReadAllLines(inputFilePath);
+
+var cardList = new List<Card>();
+foreach (string line in inputLines)
+    cardList.Add(new Card(line));
+
+int sum = cardList.Sum(c => c.GetScore());
+Console.WriteLine(sum);
