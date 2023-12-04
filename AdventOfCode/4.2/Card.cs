26cbb27c12f74b84913ed49faed102ade046ac30
Patryk Konieczny <pat366@gmail.com>
2023-12-04 20:06:33 +0100

    Day 4 challenge 2

---
+++ AdventOfCode/4.2/Card.cs
@@ -0,0 +1,28 @@
+﻿namespace _4._2
+{
+    internal class Card
+    {
+        public int[] WinningNumbers { get; }
+        public int[] MarkedNumbers { get; }
+        public int CopiesAmount { get; set; }
+
+        public Card(string cardLine)
+        {
+            string[] cardParts = cardLine.Split(':', '|');
+            WinningNumbers = cardParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            MarkedNumbers = cardParts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            CopiesAmount = 1;
+        }
+
+        public int GetScore()
+        {
+            int winningAmount = GetWonAmount();
+            if (winningAmount == 0)
+                return 0;
+            return (int)Math.Pow(2, winningAmount - 1);
+        }
+
+        public int GetWonAmount()
+            => WinningNumbers.Count(MarkedNumbers.Contains);
+    }
+}
---
+++ AdventOfCode/4.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using _4._2;
+
+Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+string[] inputLines = File.ReadAllLines(inputFilePath);
+
+var cardList = new List<Card>();
+foreach (string line in inputLines)
+    cardList.Add(new Card(line));
+
+for (int i = 0; i < cardList.Count; i++)
+{
+    Card card = cardList[i];
+    int wonCardCopiesAmount = card.GetWonAmount();
+    for (int c = 0; c < card.CopiesAmount; c++)
+    for (int j = i + 1; j < i + wonCardCopiesAmount + 1; j++)
+        cardList[j].CopiesAmount++;
+}
+
+int totalCardAmount = cardList.Sum(card => card.CopiesAmount);
+Console.WriteLine(totalCardAmount);
