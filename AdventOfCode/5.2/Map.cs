3f262d8219f5ea69156969851f024a9430a45b8c
Patryk Konieczny <pat366@gmail.com>
2023-12-05 22:53:51 +0100

    Day 5 challenge 2

---
+++ AdventOfCode/5.2/Map.cs
@@ -0,0 +1,37 @@
+﻿namespace _5._2
+{
+    internal class Map
+    {
+        private List<Entry> entries;
+
+        public Map(IEnumerable<string> inputLines)
+        {
+            entries = inputLines.Select(inputLine => inputLine.Split(' '))
+                .Select(inputLineParts => new Entry
+                {
+                    DestinationRangeStart = long.Parse(inputLineParts[0]),
+                    SourceRangeStart = long.Parse(inputLineParts[1]),
+                    RangeLength = long.Parse(inputLineParts[2])
+                })
+                .ToList();
+        }
+
+        public long GetMappedValue(long sourceValue)
+        {
+            foreach (Entry entry in entries)
+            {
+                if (sourceValue >= entry.SourceRangeStart &&
+                                       sourceValue < entry.SourceRangeStart + entry.RangeLength)
+                    return entry.DestinationRangeStart + sourceValue - entry.SourceRangeStart;
+            }
+            return sourceValue;
+        }
+
+        private class Entry
+        {
+            public long DestinationRangeStart { get; init; }
+            public long SourceRangeStart { get; init; }
+            public long RangeLength { get; init; }
+        }
+    }
+}
---
+++ AdventOfCode/5.2/Program.cs
@@ -0,0 +1,68 @@
+﻿using _5._2;
+using System.Collections.Concurrent;
+
+Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+string[] inputLines = File.ReadAllLines(inputFilePath);
+
+int lineIndex = 1;
+
+var seedToSoil = new Map(GetLinesForMap());
+var soilToFertilizer = new Map(GetLinesForMap());
+var fertilizerToWater = new Map(GetLinesForMap());
+var waterToLight = new Map(GetLinesForMap());
+var lightToTemperature = new Map(GetLinesForMap());
+var temperatureToHumidity = new Map(GetLinesForMap());
+var humidityToLocation = new Map(GetLinesForMap());
+
+IEnumerable<long> firstLineNumbers = inputLines[0].Replace("seeds: ", string.Empty)
+    .Split(' ')
+    .Select(long.Parse);
+
+var seedRanges = firstLineNumbers.Select((value, index) => (value, index))
+       .GroupBy(tuple => tuple.index / 2, tuple => tuple.value)
+       .Select(group => (start: group.First(), length: group.Last()));
+
+var locations = new ConcurrentBag<long>();
+seedRanges.AsParallel().ForAll(seedRange => GetSeedNumbers(seedRange.start, seedRange.length)
+    .AsParallel()
+       .ForAll(seedNumber => locations.Add(GetLocation(seedNumber))));
+
+
+Console.WriteLine(locations.Min());
+
+IEnumerable<long> GetSeedNumbers(long start, long length)
+{
+    for (long seedNumber = start; seedNumber < start + length; seedNumber++)
+        yield return seedNumber;
+}
+
+
+long GetLocation(long seedNumber)
+{
+    long soil = seedToSoil!.GetMappedValue(seedNumber);
+    long fertilizer = soilToFertilizer!.GetMappedValue(soil);
+    long water = fertilizerToWater!.GetMappedValue(fertilizer);
+    long light = waterToLight!.GetMappedValue(water);
+    long temperature = lightToTemperature!.GetMappedValue(light);
+    long humidity = temperatureToHumidity!.GetMappedValue(temperature);
+    long location = humidityToLocation!.GetMappedValue(humidity);
+    return location;
+}
+
+IEnumerable<string> GetLinesForMap()
+{
+    lineIndex += 2;
+    var linesForMap = new List<string>();
+    do
+    {
+        string line = inputLines[lineIndex++];
+        linesForMap.Add(line);
+    }
+    while (lineIndex < inputLines.Length && !string.IsNullOrWhiteSpace(inputLines[lineIndex]));
+
+    return linesForMap;
+}
+
