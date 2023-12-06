40b7ee98f247d24032ee9f85682dfa0386bb3c78
Patryk Konieczny <pat366@gmail.com>
2023-12-06 20:41:15 +0100

    Day 6 challenge 2

---
+++ AdventOfCode/6.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using _6._2;
+
+Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+
+// For once I'm making my input easier
+// I assume input consists of 2 numbers total
+// First line is Time
+// Second line is distance
+
+string[] inputLines = File.ReadAllLines(inputFilePath);
+var raceStats = new RaceStats
+{
+    Time = long.Parse(inputLines[0]),
+    Distance = long.Parse(inputLines[1])
+};
+
+(long rangeStart, long rangeEnd) = raceStats.GetRangeOfValidButtonHoldingTimes();
+long amountOfValidButtonHoldingTimes = rangeEnd - rangeStart + 1;
+Console.WriteLine($"Number of ways to win a race: {amountOfValidButtonHoldingTimes}");
---
+++ AdventOfCode/6.2/RaceStats.cs
@@ -0,0 +1,25 @@
+﻿namespace _6._2
+{
+    internal class RaceStats
+    {
+        public long Time { get; init; }
+        public long Distance { get; init; }
+
+        // It devolves into a quadratic inequality
+        // It's -x^2 + xy - z > 0
+        // x being our button holding time/speed
+        // y being time of the race
+        // z being distance to beat
+        public (long rangeStart, long rangeEnd) GetRangeOfValidButtonHoldingTimes()
+        {
+            double epsilon = 0.000001;
+            double delta = Math.Pow(Time, 2) - 4 * -1 * -Distance;
+            if (delta < 0)
+                throw new InvalidOperationException("No valid button holding times");
+            long x1 = (long)Math.Floor(((Time + Math.Sqrt(delta)) / 2) - epsilon);
+            long x2 = (long)Math.Ceiling(((Time - Math.Sqrt(delta)) / 2) + epsilon);
+
+            return (x2, x1);
+        }
+    }
+}
