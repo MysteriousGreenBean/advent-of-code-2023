bcf4608a30a5f74f014a52155fc133ae786d3a8b
Patryk Konieczny <pat366@gmail.com>
2023-12-06 20:34:48 +0100

    Day 6 challenge 1

---
+++ AdventOfCode/6.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using _6._1;
+
+Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+
+// For once I'm making my input easier
+// I assume input consists of 2 numbers per line, separated by a space
+// first number is time, second is distance
+
+string[] inputLines = File.ReadAllLines(inputFilePath);
+int result = 1;
+foreach (string[] lineNumbers in inputLines.Select(il => il.Split(' ')))
+{
+    var raceStats = new RaceStats
+    {
+        Time = int.Parse(lineNumbers[0]),
+        Distance = int.Parse(lineNumbers[1])
+    };
+    (int rangeStart, int rangeEnd) = raceStats.GetRangeOfValidButtonHoldingTimes();
+    int amountOfValidButtonHoldingTimes = rangeEnd - rangeStart + 1;
+    Console.WriteLine($"Number of ways to win a race: {amountOfValidButtonHoldingTimes}");
+    result *= amountOfValidButtonHoldingTimes;
+}
+Console.WriteLine(result);
---
+++ AdventOfCode/6.1/RaceStats.cs
@@ -0,0 +1,25 @@
+﻿namespace _6._1
+{
+    internal class RaceStats
+    {
+        public int Time { get; init; }
+        public int Distance { get; init; }
+
+        // It devolves into a quadratic inequality
+        // It's -x^2 + xy - z > 0
+        // x being our button holding time/speed
+        // y being time of the race
+        // z being distance to beat
+        public (int rangeStart, int rangeEnd) GetRangeOfValidButtonHoldingTimes()
+        {
+            double epsilon = 0.000001;
+            double delta = Math.Pow(Time, 2) - 4 * -1 * -Distance;
+            if (delta < 0)
+                throw new InvalidOperationException("No valid button holding times");
+            int x1 = (int)Math.Floor(((Time + Math.Sqrt(delta)) / 2) - epsilon);
+            int x2 = (int)Math.Ceiling(((Time - Math.Sqrt(delta)) / 2) + epsilon);
+
+            return (x2, x1);
+        }
+    }
+}
