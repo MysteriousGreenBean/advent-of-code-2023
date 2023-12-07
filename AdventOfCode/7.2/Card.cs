ff6317dff724171a842ea504bf2b6602f2cfb872
Patryk Konieczny <pat366@gmail.com>
2023-12-07 20:12:40 +0100

    Day 7 challenge 2

---
+++ AdventOfCode/7.2/Card.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace _7._2
+{
+    [DebuggerDisplay("{Symbol}")]
+    internal class Card
+    {
+        private static readonly Dictionary<char, int> cardValues = new()
+        {
+            { '2', 2 },
+            { '3', 3 },
+            { '4', 4 },
+            { '5', 5 },
+            { '6', 6 },
+            { '7', 7 },
+            { '8', 8 },
+            { '9', 9 },
+            { 'T', 10 },
+            { 'J', 1 },
+            { 'Q', 12 },
+            { 'K', 13 },
+            { 'A', 14 }
+        };
+
+        public char Symbol { get; init; }
+        public int Value => cardValues[Symbol];
+    }
+}
---
+++ AdventOfCode/7.2/HandVariants.cs
@@ -0,0 +1,33 @@
+﻿namespace _7._2
+{
+    internal class HandVariants
+    {
+        private readonly Hand originalHand;
+
+        public HandVariants(Hand originalHand)
+        {
+            this.originalHand = originalHand;
+        }
+
+        public Hand GetVariantWithHighestType()
+        {
+            if (originalHand.Cards.All(card => card.Symbol == 'J'))
+                return originalHand;
+
+            Hand highestTypeHand = originalHand;
+            foreach (Card nonJokerCard in originalHand.Cards.Where(card => card.Symbol != 'J').Distinct())
+            {
+                var handVariant = new Hand
+                {
+                    Cards = originalHand.Cards.Select(card => card.Symbol == 'J' ? new Card { Symbol = nonJokerCard.Symbol } : card).ToArray(),
+                    Bet = originalHand.Bet
+                };
+                if (handVariant.GetHandType() > highestTypeHand.GetHandType())
+                    highestTypeHand = handVariant;
+            }
+
+            originalHand.AdjustHandType(highestTypeHand.GetHandType());
+            return originalHand;
+        }
+    }
+}
