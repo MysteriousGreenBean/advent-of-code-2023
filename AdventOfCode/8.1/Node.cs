58245d3734cfc1d26f41a6ab3bf6947974869abb
Patryk Konieczny <pat366@gmail.com>
2023-12-08 21:59:18 +0100

    Day 8 challenge 1

---
+++ AdventOfCode/8.1/Node.cs
@@ -0,0 +1,17 @@
+﻿namespace _8._1
+{
+    internal class Node
+    {
+        public string NodeSymbol { get; }
+        public string LeftNodeSymbol { get; }
+        public string RightNodeSymbol { get; }
+
+        public Node(string nodeLine)
+        {
+            string[] nodeLineParts = nodeLine.Split(new char[] { ' ', '=', ',', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+            NodeSymbol = nodeLineParts[0];
+            LeftNodeSymbol = nodeLineParts[1];
+            RightNodeSymbol = nodeLineParts[2];
+        }
+    }
+}
---
+++ AdventOfCode/8.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using _8._1;
+
+Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+
+string[] inputLines = File.ReadAllLines(inputFilePath);
+
+
+var nodes = new Dictionary<string, Node>();
+for (int i = 2; i < inputLines.Length; i++)
+{
+    var node = new Node(inputLines[i]);
+    nodes.Add(node.NodeSymbol, node);
+}
+string instructions = inputLines[0];
+int currentInstructionIndex = 0;
+Node currentNode = nodes["AAA"];
+int steps = 0;
+while (currentNode.NodeSymbol != "ZZZ")
+{
+    if (instructions[currentInstructionIndex++] == 'L')
+        currentNode = nodes[currentNode.LeftNodeSymbol];
+    else
+        currentNode = nodes[currentNode.RightNodeSymbol];
+    if (currentInstructionIndex == instructions.Length)
+        currentInstructionIndex = 0;
+    steps++;
+}
+
+Console.WriteLine(steps);
