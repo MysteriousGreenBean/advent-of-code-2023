59e9c934a066119dcec75c6fe4bcfd001b02d207
Patryk Konieczny <pat366@gmail.com>
2023-12-09 20:52:40 +0100

    Day 9 challenge 1

---
+++ AdventOfCode/9.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using _9._1;
+
+Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+
+string[] inputLines = File.ReadAllLines(inputFilePath);
+
+var sequences = inputLines.Select(l => new Sequence(l.Split(' ').Select(n => int.Parse(n)).ToArray())).ToArray();
+
+int sumOfExtrapolatedNumbers = 0;
+foreach (var sequence in sequences)
+    sumOfExtrapolatedNumbers += sequence.ExtrapolateNextNumber();
+
+Console.WriteLine(sumOfExtrapolatedNumbers);
---
+++ AdventOfCode/9.1/Sequence.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1
+{
+    internal class Sequence
+    {
+        public int[] RecordedSequence { get; }
+
+        public Sequence(int[] recordedSequence)
+        {
+            this.RecordedSequence = recordedSequence;
+        }
+
+        public int ExtrapolateNextNumber()
+        {
+            var extrapolationSequences = new List<int[]>();
+            int[] currentExtrapolationSequence = CalculateNextExtrapolationSequence(this.RecordedSequence);
+            extrapolationSequences.Add(this.RecordedSequence);
+            extrapolationSequences.Add(currentExtrapolationSequence);
+            while (!currentExtrapolationSequence.All(n => n == 0))
+            {
+                currentExtrapolationSequence = CalculateNextExtrapolationSequence(currentExtrapolationSequence);
+                extrapolationSequences.Add(currentExtrapolationSequence);
+            }
+
+            return ExtrapolateNextNumber(extrapolationSequences);
+        }
+
+        private int[] CalculateNextExtrapolationSequence(int[] extrapolationSequence)
+        {
+            var result = new int[extrapolationSequence.Length - 1];
+            for (int i = 1; i < extrapolationSequence.Length; i++)
+                result[i - 1] = extrapolationSequence[i] - extrapolationSequence[i - 1];
+            return result;
+        }
+
+        private int ExtrapolateNextNumber(List<int[]> extrapolationSequences)
+        {
+            for (int i = extrapolationSequences.Count - 1; i >= 0; i--)
+            {
+                extrapolationSequences[i] = extrapolationSequences[i].Append(ExtrapolateNextNumber(extrapolationSequences[i], i == extrapolationSequences.Count - 1 ? null : extrapolationSequences[i + 1])).ToArray();
+            }
+            return extrapolationSequences[0].Last();
+        }
+
+        private int ExtrapolateNextNumber(int[] extrapolatedSequence, int[]? lowerExtrapolationSequence)
+        {
+            if (extrapolatedSequence.All(n => n == 0))
+                return 0;
+
+            if (lowerExtrapolationSequence == null)
+                throw new ArgumentNullException(nameof(lowerExtrapolationSequence));
+
+            return extrapolatedSequence.Last() + lowerExtrapolationSequence.Last();
+        }
+    }
+}
