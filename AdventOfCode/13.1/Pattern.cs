2e865f42fb2d7a0d3cbda22ac041da5f7ff366a9
Patryk Konieczny <pat366@gmail.com>
2023-12-13 20:55:31 +0100

    formatting

--- AdventOfCode/13.1/Pattern.cs
+++ AdventOfCode/13.1/Pattern.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _13._1
+﻿namespace _13._1
 {
     internal class Pattern
     {
