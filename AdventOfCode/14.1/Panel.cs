9b0428e2bd5510f3bebfeb949cddced7636e85bb
Patryk Konieczny <pat366@gmail.com>
2023-12-14 17:49:21 +0100

    Day 14 challenge 1

---
+++ AdventOfCode/14.1/Panel.cs
@@ -0,0 +1,49 @@
+﻿namespace _14._1
+{
+    internal class Panel
+    {
+        private char[][] panelLines;
+        public Panel(string[] panelLines) {
+            this.panelLines = panelLines.Select(s => s.ToCharArray()).ToArray();
+        }
+
+        public void TiltNorth()
+        {
+            for (int i = 1; i < panelLines.Length; i++)
+            {
+                for (int j = 0; j < panelLines[0].Length; j++)
+                {
+                    if (panelLines[i][j] == 'O')
+                    {
+                        int northIndex = i - 1;
+                        int currentIndex = i;
+                        while (northIndex >= 0 && panelLines[northIndex][j] == '.')
+                        {
+                            panelLines[currentIndex--][j] = '.';
+                            panelLines[northIndex--][j] = 'O';
+                        }
+                    }
+                }
+            }
+        }
+
+        public int CalculateLoad()
+        {
+            int multiplier = panelLines.Length;
+            int load = 0;
+            foreach (string line in panelLines.Select(c => new string(c)))
+                load += line.Count(c => c == 'O') * multiplier--;
+            return load;
+        }
+
+        public void Print()
+        {
+            foreach (var line in panelLines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AdventOfCode/14.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using _14._1;
+
+Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+
+string[] inputLines = File.ReadAllLines(inputFilePath);
+Panel panel = new Panel(inputLines);
+panel.Print();
+panel.TiltNorth();
+panel.Print();
+Console.WriteLine($"Load: {panel.CalculateLoad()}");
