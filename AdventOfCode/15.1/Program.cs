59435efdc07d78dc9dae1ed278b6b91fe6e53a7a
Patryk Konieczny <pako@omada.net>
2023-12-15 09:36:41 +0100

    Day 15 challenge 1

---
+++ AdventOfCode/15.1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+
+//read input file by chars using streams
+using FileStream inputStream = File.OpenRead(inputFilePath);
+using StreamReader streamReader = new StreamReader(inputStream);
+
+long sum = 0;
+int currentValue = 0;
+while (!streamReader.EndOfStream)
+{
+    int charValue = streamReader.Read();
+    if (charValue == (int)'\n')
+        continue;
+    if (charValue == ',')
+    {
+        sum += currentValue;
+        currentValue = 0;
+        continue;
+    }
+    currentValue += charValue;
+    currentValue *= 17;
+    currentValue = currentValue % 256;
+}
+sum += currentValue;
+
+Console.WriteLine(
+    $"Sum is {sum}");
