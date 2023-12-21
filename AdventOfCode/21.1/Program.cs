9e1baa9ff389ebdfe5c3be6b1ac2728e14f29521
Patryk Konieczny <pat366@gmail.com>
2023-12-21 22:12:44 +0100

    Clean up

--- AdventOfCode/21.1/Program.cs
+++ AdventOfCode/21.1/Program.cs
@@ -1,11 +1,10 @@
 ﻿using _21._1;
 
 Console.WriteLine("Provide path to input file:");
-string? inputFilePath = "C:\\Users\\Patryk\\Downloads\\input.txt";
+string? inputFilePath = Console.ReadLine(); ;
 if (!File.Exists(inputFilePath))
     throw new InvalidDataException("Provided file does not exist");
 
 string[] inputLines = File.ReadAllLines(inputFilePath);
 var map = new Map(inputLines);
-Console.WriteLine($"Possible targets: {map.PossibleTargets(64)}");
-int steps = 1;
+Console.WriteLine($"Possible targets: {map.PossibleTargets(6)}");
