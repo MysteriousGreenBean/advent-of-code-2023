b34bf238723ebfcbe98ce9dec4c9596c1886fc6c
Patryk Konieczny <pat366@gmail.com>
2023-12-22 13:00:19 +0100

    Day 22 challenge 1

---
+++ AdventOfCode/22.1/Brick.cs
@@ -0,0 +1,82 @@
+﻿namespace _22._1
+{
+    internal class Brick
+    {
+        private static int idCounter = 1;
+        private readonly int id = idCounter++;
+        public Coords Start { get; private set; }
+        public Coords End { get; private set; }
+
+        public Brick(Coords start, Coords end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public void MoveDown()
+        {
+            Start = Start.MoveDown();
+            End = End.MoveDown();
+        }
+
+        public bool IsAnythingBeneathMe(Brick[] otherBricks)
+        {
+            if (StandsOnGround())
+            {
+                return true;
+            }
+
+            foreach (var otherBrick in otherBricks)
+            {
+                if (otherBrick == this)
+                {
+                    continue;
+                }
+
+                if (IsBeneath(otherBrick))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsBeneath(Brick otherBrick)
+        {
+            if (otherBrick == this)
+                return false;
+
+            if (otherBrick.Start.Z + 1 == End.Z || otherBrick.End.Z + 1 == End.Z || otherBrick.Start.Z + 1 == Start.Z || otherBrick.End.Z + 1 == Start.Z)
+                return RangesCross(otherBrick.Start.X, otherBrick.End.X, Start.X, End.X)
+                       && RangesCross(otherBrick.Start.Y, otherBrick.End.Y, Start.Y, End.Y);
+
+            return false;
+        }
+
+        private bool StandsOnGround()
+            => Start.Z == 1 || End.Z == 1;
+
+        private bool RangesCross(int start1, int end1, int start2, int end2)
+            => Enumerable.Range(start1, end1 - start1 + 1).Intersect(Enumerable.Range(start2, end2 - start2 + 1)).Any();
+
+        public override string ToString()
+            => $"{id}:({Start}~{End})";
+    }
+
+    internal record Coords(int X, int Y, int Z)
+    {
+        public Coords MoveDown()
+            => this with { Z = Z - 1 };
+
+        public override string ToString()
+            => $"({X}, {Y}, {Z})";
+    }
+
+    internal enum Axis
+    {
+        X,
+        Y,
+        Z
+    }
+}
---
+++ AdventOfCode/22.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using _22._1;
+
+Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+
+string[] inputLines = File.ReadAllLines(inputFilePath);
+var bricks = inputLines.Select(line =>
+{
+    string[] splitLines = line.Split('~');
+    string[] startCoords = splitLines[0].Split(',');
+    string[] endCoords = splitLines[1].Split(',');
+    return new Brick(new Coords(int.Parse(startCoords[0]), int.Parse(startCoords[1]), int.Parse(startCoords[2])),
+               new Coords(int.Parse(endCoords[0]), int.Parse(endCoords[1]), int.Parse(endCoords[2])));
+}).ToArray();
+
+
+while (bricks.Any(b => !b.IsAnythingBeneathMe(bricks)))
+{
+    foreach (var brick in bricks)
+    {
+        if (!brick.IsAnythingBeneathMe(bricks))
+            brick.MoveDown();
+    }
+}
+
+int canBeDisintegrated = 0;
+foreach (var brick in bricks)
+{
+    var arrayCopy = bricks.Except(new[] { brick }).ToArray();
+    bool areAllSafe = true;
+    foreach (var brick2 in arrayCopy)
+    {
+        if (!brick2.IsAnythingBeneathMe(arrayCopy))
+            areAllSafe = false;
+    }
+    if (areAllSafe)
+        canBeDisintegrated++;
+}
+
+Console.WriteLine($"Can be disintegrated: {canBeDisintegrated}");
