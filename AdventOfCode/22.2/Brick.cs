421818308052d6391783ea88801143830cb502ad
Patryk Konieczny <pat366@gmail.com>
2023-12-22 15:54:09 +0100

    Day 22 Challenge 2

---
+++ AdventOfCode/22.2/Brick.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _22._2
+{
+    internal class Brick
+    {
+        private static int idCounter = 1;
+        private readonly int id = idCounter++;
+        public Coords Start { get; private set; }
+        public Coords End { get; private set; }
+
+        public Brick(Coords start, Coords end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public void MoveDown()
+        {
+            Start = Start.MoveDown();
+            End = End.MoveDown();
+        }
+
+        public bool IsAnythingBeneathMe(Brick[] otherBricks)
+        {
+            if (StandsOnGround())
+            {
+                return true;
+            }
+
+            foreach (var otherBrick in otherBricks)
+            {
+                if (otherBrick == this)
+                {
+                    continue;
+                }
+
+                if (IsBeneath(otherBrick))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsBeneath(Brick otherBrick)
+        {
+            if (otherBrick == this)
+                return false;
+
+            if (otherBrick.Start.Z + 1 == End.Z || otherBrick.End.Z + 1 == End.Z || otherBrick.Start.Z + 1 == Start.Z || otherBrick.End.Z + 1 == Start.Z)
+                return RangesCross(otherBrick.Start.X, otherBrick.End.X, Start.X, End.X)
+                       && RangesCross(otherBrick.Start.Y, otherBrick.End.Y, Start.Y, End.Y);
+
+            return false;
+        }
+
+        private bool StandsOnGround()
+            => Start.Z == 1 || End.Z == 1;
+
+        private bool RangesCross(int start1, int end1, int start2, int end2)
+            => Enumerable.Range(start1, end1 - start1 + 1).Intersect(Enumerable.Range(start2, end2 - start2 + 1)).Any();
+
+        public override string ToString()
+            => $"{(char)(id+64)}:({Start}~{End})";
+    }
+
+    internal class BrickComparer : IEqualityComparer<Brick>
+    {
+        public bool Equals(Brick? x, Brick? y)
+        {
+            if (x is null && y is not null)
+                return false;
+            if (x is not null && y is null)
+                return false;
+            if (x is null && y is null)
+                return true;
+
+            if (x!.Start == y!.Start && x!.End == y!.End)
+                return true;
+
+            if (x!.Start == y!.End && x!.End == y!.Start)
+                return true;
+
+            return false;
+        }
+
+        public int GetHashCode([DisallowNull] Brick obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    internal record Coords(int X, int Y, int Z)
+    {
+        public Coords MoveDown()
+            => this with { Z = Z - 1 };
+
+        public override string ToString()
+            => $"({X}, {Y}, {Z})";
+    }
+
+    internal enum Axis
+    {
+        X,
+        Y,
+        Z
+    }
+}
---
+++ AdventOfCode/22.2/Program.cs
@@ -0,0 +1,70 @@
+﻿using _22._2;
+
+Console.WriteLine("Provide path to input file:");
+string? inputFilePath = Console.ReadLine();
+if (!File.Exists(inputFilePath))
+    throw new InvalidDataException("Provided file does not exist");
+
+string[] inputLines = File.ReadAllLines(inputFilePath);
+var bricks = inputLines.Select(line =>
+{
+    string[] splitLines = line.Split('~');
+    string[] startCoords = splitLines[0].Split(',');
+    string[] endCoords = splitLines[1].Split(',');
+    return new Brick(new Coords(int.Parse(startCoords[0]), int.Parse(startCoords[1]), int.Parse(startCoords[2])),
+               new Coords(int.Parse(endCoords[0]), int.Parse(endCoords[1]), int.Parse(endCoords[2])));
+}).ToArray();
+
+
+while (bricks.Any(b => !b.IsAnythingBeneathMe(bricks)))
+{
+    foreach (var brick in bricks)
+    {
+        if (!brick.IsAnythingBeneathMe(bricks))
+            brick.MoveDown();
+    }
+}
+
+long totalFellDownAmount = 0;
+var bricksFellCache = new Dictionary<Brick, HashSet<Brick>>();
+int processedBricks = 0;
+foreach (var brick in bricks.OrderByDescending(b => Math.Max(b.Start.Z, b.End.Z)))
+{
+    var arrayCopy = bricks.Except(new[] { brick }).ToArray();
+    var fellBricks = new HashSet<Brick>(new BrickComparer());
+
+    var readFromCache = new HashSet<Brick>(new BrickComparer());
+    foreach (var otherBrick in arrayCopy)
+    {
+        if (!otherBrick.IsAnythingBeneathMe(arrayCopy) && bricksFellCache.ContainsKey(otherBrick))
+        {
+            fellBricks.Add(otherBrick);
+            readFromCache.Add(otherBrick);
+            fellBricks.UnionWith(bricksFellCache[otherBrick]);
+        }
+    }
+
+    var nonCachedBricks = arrayCopy.Except(readFromCache).ToArray();
+
+    while (nonCachedBricks.Any(b => !b.IsAnythingBeneathMe(nonCachedBricks)))
+    {
+        foreach (var otherBrick in nonCachedBricks)
+        {
+            if (!otherBrick.IsAnythingBeneathMe(nonCachedBricks))
+            {
+                otherBrick.MoveDown();
+                fellBricks.Add(otherBrick);
+            }
+        }
+    }
+
+    processedBricks++;
+    if (!bricksFellCache.ContainsKey(brick))
+        bricksFellCache.Add(brick, fellBricks);
+    else
+        bricksFellCache[brick].UnionWith(fellBricks);
+
+    totalFellDownAmount += fellBricks.Count;
+}
+
+Console.WriteLine($"Sum of fell down: {totalFellDownAmount}");
